dc1810decf173e2e7ab3e617731a290399ee5e86
Tristan <tristanshaw000@gmail.com>
2024-08-15 16:29:51 -0400

    oops

--- src/utils/about_v12.rs
+++ src/utils/about_v12.rs
@@ -12,7 +12,7 @@ pub(crate) fn about_v12() {
   print_c(colors::MAGENTA, styles::BOLD, banner);
   n_line();
   print_c(colors::MAGENTA, styles::BOLD, " About V12 - A simple, lightweight, and easy-to-use JavaScript Interpreter/Engine written in Rust.\n");
-  print_c(colors::MAGENTA, styles::BOLD, " Version: 0.4.0\n");
+  print_c(colors::MAGENTA, styles::BOLD, " Version: 0.5.0\n");
   print_c(colors::MAGENTA, styles::BOLD, " Author(s): Thoq\n");
   print_c(colors::MAGENTA, styles::BOLD, " License: MIT\n");
   print_c(colors::MAGENTA, styles::BOLD, " Build: 81424 (development)\n");
