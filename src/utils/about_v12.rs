bd019ec2df2821be17f99b228d6aae9f367e9bc4
Tristan <tristanshaw000@gmail.com>
2024-10-04 15:56:23 -0400

    fix version number and description

--- src/utils/about_v12.rs
+++ src/utils/about_v12.rs
@@ -11,11 +11,11 @@ pub(crate) fn about_v12() {
 "#;
   print_c(colors::MAGENTA, styles::BOLD, banner);
   n_line();
-  print_c(colors::MAGENTA, styles::BOLD, " About V12 - A simple, lightweight, and easy-to-use JavaScript Interpreter/Engine written in Rust.\n");
-  print_c(colors::MAGENTA, styles::BOLD, " Version: 0.5.0\n");
+  print_c(colors::MAGENTA, styles::BOLD, " About V12 - A simple, lightweight, and easy-to-use JavaScript Runtime written in Rust.\n");
+  print_c(colors::MAGENTA, styles::BOLD, " Version: 0.5.1\n");
   print_c(colors::MAGENTA, styles::BOLD, " Author(s): Thoq\n");
   print_c(colors::MAGENTA, styles::BOLD, " License: MIT\n");
-  print_c(colors::MAGENTA, styles::BOLD, " Build: 81424 (development)\n");
+  print_c(colors::MAGENTA, styles::BOLD, " Build: 10424 (development)\n");
   n_line();
   print_c(colors::MAGENTA, styles::BOLD, " HELP:\n");
   print_c(colors::MAGENTA, styles::BOLD, "   Usage: V12 <script_path>.ts/.js, version [debug]\n");
